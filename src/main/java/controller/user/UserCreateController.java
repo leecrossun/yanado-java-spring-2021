370ea16e89c638664aa1bfb8bcbf3630a1a000d9
todayiswindy <72553506+todayiswindy@users.noreply.github.com>
2021-05-10 21:13:00 +0900

    [ADD] UserController, MypageController

---
+++ src/main/java/controller/user/UserCreateController.java
@@ -0,0 +1,32 @@
+package controller.user;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// ȸ�� �߰� �۾� - �Ķ���� �޾Ƽ� ȸ�� ����Ʈ�� �ѱ�
+@WebServlet("/user/insert")
+public class UserCreateController extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+
+		String userId = request.getParameter("name");
+		String password = request.getParameter("pasword");
+		String userName = request.getParameter("userName");
+		String gender = request.getParameter("gender");
+		String birth = request.getParameter("birth");
+		String address = request.getParameter("request");
+		String phoneNumber = request.getParameter("phoneNumber");
+		String email = request.getParameter("email");
+//		int rankCount = request.getParameter("rankCount");
+		
+		response.sendRedirect("/user/user_login.jsp");
+	}
+} 
---
+++ src/main/java/controller/user/UserDeleteController.java
@@ -0,0 +1,30 @@
+package controller.user;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import persistence.dao.UserDAO;
+
+// ȸ�� ���� �۾�
+@WebServlet("/user/delete")
+public class UserDeleteController extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+
+		String userId = request.getParameter("userId");
+		int deleteCode = UserDAO.getInstance().deleteUser(userId);	// 1�̸� ����, 0�̸� ����
+		System.out.println("deleteCode: " + deleteCode);
+
+		//json ��ȯ
+		String result = String.format("[{'res' : '%d'}, {'id' : '%s'}]", deleteCode, userId);
+		response.getWriter().println(result);
+	}
+}
