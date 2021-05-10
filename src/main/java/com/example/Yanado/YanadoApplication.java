b7d28798813fa09ee06fe66ae92915657deeb68c
hyosunLee <leecrossun@gmail.com>
2021-05-10 19:10:36 +0900

    "init"

---
+++ src/main/java/com/example/Yanado/YanadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Yanado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YanadoApplication.class, args);
+	}
+
+}
