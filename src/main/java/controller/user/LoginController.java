c95c3835a365df31ad4d6ef6fc3aa156805bcc9c
todayiswindy <dpswpfkgy@gmail.com>
2021-05-26 01:19:02 +0900

    [FEAT][MODIFY] ProductDAO, UserDAO, 관련 Controller

--- src/main/java/controller/user/LoginController.java
+++ src/main/java/controller/user/LoginController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import persistence.dao.UserDAO;
-import service.dto.UserDTO;
+import service.dto.User;
 
 @WebServlet("/user/login")
 public class LoginController extends HttpServlet {
@@ -25,7 +25,7 @@ public class LoginController extends HttpServlet {
 		String userId = request.getParameter("userId");
 		String password = request.getParameter("password");
 
-		UserDTO dto = UserDAO.getInstance().getUserByUserId(userId);
+		User dto = UserDAO.getInstance().getUserByUserId(userId);
 		
 		String param = ""; // param의 값
 		String resultStr = ""; // 전송될 json 문자열
