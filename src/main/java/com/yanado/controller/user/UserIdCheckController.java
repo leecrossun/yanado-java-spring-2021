c5e79736986ac61901679c3166ffb1de9dc9689e
todayiswindy <dpswpfkgy@gmail.com>
2021-06-18 00:12:35 +0900

    [MODIFY] UserIdCheckController

--- src/main/java/com/yanado/controller/user/UserIdCheckController.java
+++ src/main/java/com/yanado/controller/user/UserIdCheckController.java
@@ -2,11 +2,13 @@ package com.yanado.controller.user;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.yanado.dao.UserDAO;
 import com.yanado.dto.User;
@@ -16,8 +18,8 @@ public class UserIdCheckController extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		String id = request.getParameter("id");
 		User dto = UserDAO.getUserByUserId(id); 
