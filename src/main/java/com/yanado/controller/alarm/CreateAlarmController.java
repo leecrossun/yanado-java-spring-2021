7952f6caf074c2e4f753980b636d0d0d2753b8ae
HYUNJEONGMUN <s205044s@naver.com>
2021-06-17 22:44:02 +0900

    mypage alarm & common

--- src/main/java/com/yanado/controller/alarm/CreateAlarmController.java
+++ src/main/java/com/yanado/controller/alarm/CreateAlarmController.java
@@ -24,39 +24,38 @@ import com.yanado.dto.CommonJoin;
 import com.yanado.dto.Product;
 import com.yanado.service.CommonService;
 
-
 @Controller
 @SessionAttributes("alarm")
 public class CreateAlarmController {
 	@Autowired
 	private AlarmDAO alarmDao;
-	
+
 	@Autowired
 	private CommonService commonService;
-	
+
 	// 알람 생성 폼으로 가기
 	@ModelAttribute("alarm")
 	public Alarm formBacking(HttpServletRequest request) {
 		Alarm alarm = new Alarm();
 		// UserSessionUtils uSession = new UserSessionUtils();
 		// String userId = uSession.getLoginUserId(request.getSession());
-		
+
 		int flag = Integer.parseInt(request.getParameter("flag"));
-		
-		if(flag == 1) {
+
+		if (flag == 1) {
 			String commonId = request.getParameter("commonId");
 			Common common = commonService.findCommonByCommonId(commonId);
 			Product product = commonService.findProduct(common.getProductId()); // 나중에 수정
-			
+
 			alarm.setDeadline(common.getDeadline());
 			alarm.setPrice(product.getPrice());
 			alarm.setCommonId(commonId);
 			alarm.setAucId(null);
-			
+
 		} else {
 			// 옥션일 때 채우기
 		}
-		
+
 		return alarm;
 	}
 
@@ -67,24 +66,27 @@ public class CreateAlarmController {
 
 	// 알람 생성하기
 	@RequestMapping(value = "/alarm/create", method = RequestMethod.POST)
-	public String createalarm(@Valid @ModelAttribute("alarm") Alarm newalarm, BindingResult result, RedirectAttributes red,
-			SessionStatus status) {
-		
+	public String createalarm(@Valid @ModelAttribute("alarm") Alarm newalarm, BindingResult result,
+			RedirectAttributes red, SessionStatus status) {
+
 		if (result.hasErrors()) {
 			return "/alarm/alarmForm";
 		}
 
 		status.setComplete();
-		
-		List<CommonJoin> commonJoin = commonService.findAllCommonJoinByCommonId(newalarm.getCommonId());
-		for (CommonJoin j : commonJoin) {
-			
-			newalarm.setUserId(j.getUserId());
-			alarmDao.insertAlarm(newalarm);
+
+		if (newalarm.getCommonId() != null) {
+			List<CommonJoin> commonJoin = commonService.findAllCommonJoinByCommonId(newalarm.getCommonId());
+			for (CommonJoin j : commonJoin) {
+
+				newalarm.setUserId(j.getUserId());
+				alarmDao.insertAlarm(newalarm);
+			}
+		} else {
+			// auction
 		}
-		
-		
-		if(newalarm.getCommonId() != null) {
+
+		if (newalarm.getCommonId() != null) {
 			red.addAttribute("commonId", newalarm.getCommonId());
 			return "redirect:/common/alarm/list";
 		} else {
