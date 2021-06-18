c4702061e671c65beb1fb406c145478475238ca1
asdfq <asdfq@DESKTOP-E126RK5>
2021-06-19 08:08:36 +0900

    auc

--- src/main/java/com/yanado/controller/auc/CreateAucController.java
+++ src/main/java/com/yanado/controller/auc/CreateAucController.java
@@ -1,32 +1,40 @@
 package com.yanado.controller.auc;
 
+import com.yanado.dto.Auc;
+import com.yanado.service.AucService;
+
 @Controller
 @SessionAtrributes("auc")
-@RequestMapping("auc/create")
+@RequestMapping("/auc/create")
 public class CreateAucController {
 	   @Autowired
-	   private AucDAO aucDAO;
+	   private AucService aucService;
 	   
 	   @ModelAttribute("auc")
 	   public Auc formBacking(HttpServletRequest request) {
 	      Auc auc = new Auc();
+	      String userId = "admin";
+	      auc.setuserId(userId);
+	      auc.setstatus(0);
 
 	      return auc;
 	   }
-	   @RequestMapping(value='auc/register',method=RequestMethod.GET)
+	   
+	   @RequestMapping(value='/auc/create',method=RequestMethod.GET)
 	   public String from() {
 	      return 'auc/form';
 	   }
-	   @RequestMapping(value = "auc/register", method = RequestMethod.POST)
+	   
+	   @RequestMapping(value = "/auc/create", method = RequestMethod.POST)
 	   public String createShopping(@Valid @ModelAttribute("auc") Auc auc, BindingResult result,
 	         SessionStatus status) {
 
 	      if (result.hasErrors()) {
 	         return "auc/form";
 	      }
-
+	      aucService.createAuc(auc);
 	      status.setComplete();
-	      return "auc/created";
+	      return "redirect:/auc/";
 	   }
 
 }
--- src/main/java/com/yanado/controller/auc/DeleteAucController.java
+++ src/main/java/com/yanado/controller/auc/DeleteAucController.java
@@ -1,5 +1,15 @@
 package com.yanado.controller.auc;
 
-public class DeleteAucController {
+import org.springframework.beans.factory.annotation.Autowired;
 
+@Controller
+public class DeleteAucController {
+	@Autowired
+	private AucService aucService;
+	
+	@RequestMapping("/auc/delete")
+	public String delete(@RequestParam String aucNo) {
+		aucService.deleteAuc(aucNo);
+		return "redirect:/auc/list";
+	}
 }
