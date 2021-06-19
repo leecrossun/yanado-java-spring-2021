9c09379702f06921fa1caa44c0ee5f5256ed40c4
todayiswindy <dpswpfkgy@gmail.com>
2021-06-19 15:43:26 +0900

    [FEAT] LoginPage

--- src/main/java/com/yanado/controller/user/UserIdCheckController.java
+++ src/main/java/com/yanado/controller/user/UserIdCheckController.java
@@ -2,13 +2,11 @@ package com.yanado.controller.user;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -18,12 +16,8 @@ import com.yanado.dto.User;
 @WebServlet("/user/checkId")
 public class UserIdCheckController extends HttpServlet {
 	
-	@Autowired
-	UserDAO userDAO;
 	private static final long serialVersionUID = 1L;
 	
-	@Autowired private UserDAO userDao;
-
 	@Autowired
 	private UserDAO userDAO;
 	
