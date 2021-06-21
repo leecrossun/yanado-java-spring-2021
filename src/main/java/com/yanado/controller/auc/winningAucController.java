3ddc430164c9fc690155f6b76d7cd785887e7ed2
asdfq <asdfq@DESKTOP-E126RK5>
2021-06-21 16:37:03 +0900

    ?

--- src/main/java/com/yanado/controller/auc/winningAucController.java
+++ src/main/java/com/yanado/controller/auc/winningAucController.java
@@ -1,5 +1,7 @@
 package com.yanado.controller.auc;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.yanado.dto.AucJoin;
 import com.yanado.service.AucService;
 
 @Controller
@@ -15,8 +18,18 @@ public class winningAucController {
 	@Autowired
 	private AucService aucService;
 	
-	public String search(HttpServletRequest request,@RequestParam String aucNo, Object userId) {
-		aucService.getBuyerListByAucNo(int userId);
-		return null;
+	public String search(HttpServletRequest request,@RequestParam String aucId) {
+		List<AucJoin> list = aucService.getAucJoinByAucId(aucId);
+		int high = 0;
+		int num=0;
+		String name="";
+		for (AucJoin a : list) {
+			 num = a.getBidPrice();
+			if(num>high) {
+				high = num;
+				name = a.getUserId();
+			}
+		}
+		return name;
 	}
 }
