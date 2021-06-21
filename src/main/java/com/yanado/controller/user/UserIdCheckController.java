134c01e4c709add2b746b01c0e0d20768c9b7a76
MHJ <MHJ@DESKTOP-9VOOU2B>
2021-06-21 14:28:26 +0900

    로그인

--- src/main/java/com/yanado/controller/user/UserIdCheckController.java
+++ src/main/java/com/yanado/controller/user/UserIdCheckController.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.yanado.dao.UserDAO;
@@ -17,27 +19,33 @@ import com.yanado.dto.User;
 
 @Controller
 @WebServlet("/user/checkId")
-public class UserIdCheckController{
-	
+public class UserIdCheckController {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Autowired
 	private UserDAO userDAO;
 
+	@RequestMapping("/user/checkId")
 	protected String service(HttpServletRequest request, HttpServletResponse response, RedirectAttributes red)
 			throws ServletException, IOException {
 
 		String id = request.getParameter("userId");
-		User dto = userDAO.getUserByUserId(id); 
-		
+		System.out.println(id);
+		User dto = userDAO.getUserByUserId(id);
+
 		int exist = 1;
 
 		if (dto == null) {
 			exist = 0;
 		}
-		
+
 		red.addAttribute("exist", exist);
-		red.addAttribute("checked", true);
+
+		if (exist == 0) {
+			red.addAttribute("userId", id);
+		}
+
 		return "redirect:/user/create";
 	}
 }
